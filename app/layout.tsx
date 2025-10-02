e68823cec271b3f81e269d7bbf91b760c83e9c16
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-10-02 14:52:35 +1000

    chore(fonts): customise fonts
    - Customisation of Radix UI them to set up variable options for other type of fonts.
    - Creation of 'theme-config.ts' file to customise fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 import "@radix-ui/themes/styles.css";
-import type { Metadata } from "next";
+import './theme-config.css';
 import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
 import { Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
+
+const inter = Inter({
+  subsets: ["latin"],
+  // display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
-        <Theme>
+        <Theme accentColor="cyan" grayColor="mauve" panelBackground="solid" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
