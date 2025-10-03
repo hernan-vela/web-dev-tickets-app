5035703244e0fd70b1e6ba4682b4791ba0def29a
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-10-03 15:47:54 +1000

    Refactor(new issue): move inline function

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -11,9 +11,7 @@ role="status">
   className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
   >Loading...</span>
 </div>
-
-
-  );
+);
 }
 
 export default Spinner;
--- app/issues/newIssue/page.tsx
+++ app/issues/newIssue/page.tsx
@@ -26,6 +26,17 @@ const NewIssuePage = () => {
   const [ error, setError ] = useState('');
   const [ isSubmitting, setSubmitting ] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues'); 
+    } catch (error) {
+      setSubmitting(false);  
+      setError('An unexpected error occurred.')
+      }
+    });
+
   return(
     <div className='max-w-xl'>
       {error && <Callout.Root color='red' className='mb-5'>
@@ -34,19 +45,10 @@ const NewIssuePage = () => {
 	      </Callout.Icon>
         <Callout.Text>{error}</Callout.Text>
         </Callout.Root>}
-
     <form 
     className='space-y-5' 
-    onSubmit={handleSubmit(async (data) => {
-      try {
-        setSubmitting(true);
-        await axios.post('/api/issues', data);
-        router.push('/issues'); 
-      } catch (error) {
-        setSubmitting(false);  
-        setError('An unexpected error occurred.')
-      }
-    })}>
+    onSubmit={onSubmit}
+    >
       <TextField.Root placeholder='Title' {...register('title')}/>
       <ErrorMessage>
       {errors.title?.message}
