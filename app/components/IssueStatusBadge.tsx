3f970c5149f0390a490378f59587efd8dc5bff2b
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-10-07 12:01:43 +1000

    feat(issues): Build the issue status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,30 @@
+
+import { Status } from '@/generated/prisma/enums';
+import { Badge } from '@radix-ui/themes';
+import React from 'react';
+
+/**
+ * This interface definition can be omitted because
+ * the props in the arguments is just one line, so the code is simpler to understand
+ */
+// interface Props {
+//   status: Status
+// }
+
+const statusMap: Record<
+  Status, 
+  { label: string, color: 'red' | 'violet' | 'green'}
+> = {
+    OPEN: { label: 'open', color: 'red' },
+    IN_PROGRESS: { label: 'In Progress', color: 'violet' },
+    CLOSED: { label: 'Closed', color: 'green' }
+  };
+  
+const IssueStatusBadge = ( { status }:  { status: Status }) => {
+  return (
+    <Badge color={statusMap[status].color}>
+      {statusMap[status].label}</Badge>
+  );
+}
+
+export default IssueStatusBadge;
