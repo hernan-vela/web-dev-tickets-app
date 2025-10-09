7355cd7c73575473118987876b1da5b414b74e68
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-10-09 11:43:50 +1000

    Add additional loading skeletons for 'new issue' and 'issues' list.

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Box, Card, Flex } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading...</div>
+    <Box className='max-w-xl'>
+  <Skeleton />
+    <Flex className='gap-3 items-center' my='2'>
+      <Skeleton width='5rem'/>
+      <Skeleton width='8rem' />
+    </Flex>
+    <Card>
+  <Skeleton count={3} />
+    </Card>
+    </Box>
   )
 }
 
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import { prisma } from '@/prisma/client';
 import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import delay from 'delay';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 
@@ -15,6 +16,8 @@ const IssueDetailsPage = async ({ params }: Props) => {
 
    if(!issue)
       notFound();
+   // Delay to see the skeletons in dev mode. It can be removed in production
+   await delay(2000);
 
   return (
     <div>
