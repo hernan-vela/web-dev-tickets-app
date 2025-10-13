5b8687442967e5bf13873963d81b07e63c986307
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-10-14 08:03:56 +1000

    feat(edit issue): Show a confirmation dialog box to delete an issue.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,33 @@
+'use client';
+
 import { EraserIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return(
-    <Button color='gold'>
-      <EraserIcon />
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+        <EraserIcon />
+        Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+      <AlertDialog.Description>
+        Wait, wait, wait! If you delete this, it cannot be undone. Last word?
+      </AlertDialog.Description>
+      <Flex mt='5' gap='4'>
+        <AlertDialog.Cancel>
+          <Button variant='soft' color='gray'>Cancel</Button>
+        </AlertDialog.Cancel>
+        <AlertDialog.Action>
+          <Button color='red'>Delete Issue</Button>
+        </AlertDialog.Action>
+      </Flex>
+      </AlertDialog.Content>
+      
+    </AlertDialog.Root>
   );
 
 }
