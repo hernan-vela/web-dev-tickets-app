0e4a3fc2938891cb635be24e288789abad495e37
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-10-14 15:19:12 +1000

    refactor(issue delete): improve the user experience by adding a spinner and disabling the 'Delete Issue' button when the user deletes an issue.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Spinner } from "@/app/components";
 import { EraserIcon } from "@radix-ui/react-icons";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
@@ -9,12 +10,16 @@ import { useState } from "react";
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
+
   const deleteIssue = async () => {
     try {
+      setDeleting(true);
       await axios.delete('/api/issues/' + issueId);
       router.push('/issues');
       router.refresh();
     } catch (error) {
+      setDeleting(false);
       setError(true);
     }
   }
@@ -23,9 +28,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <>
     <AlertDialog.Root>
       <AlertDialog.Trigger>
-        <Button color='red'>
+        <Button color='red' disabled={isDeleting}>
         <EraserIcon />
         Delete Issue
+        {isDeleting && <Spinner />}
         </Button>
       </AlertDialog.Trigger>
       <AlertDialog.Content>
